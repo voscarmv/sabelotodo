9b0ec550e2ccf2d6c8309cdc2282741593583cce
Oscar Mier <voscarmv@gmail.com>
2025-08-15 22:48:07 -0500

    First STT round success. Now feed it to qwen

--- listener.sh
+++ listener.sh
@@ -7,14 +7,18 @@ LISTENING=true
 voice2json --profile ~/.local/share/voice2json/es2 transcribe-stream --open | \
 # voice2json transcribe-stream --open | \
 while read line ; do
-            # echo $line
+    echo $line
     # if $LISTENING ; then
         if node wakeword.js "$line" ; then
             # LISTENING=false
             espeak -p 0 -v spanish 'que onda'
-            break
+            voice2json --profile ~/.local/share/voice2json/es_kaldi-rhasspy record-command > output.wav
+            whisper -o . -f json --language es --model tiny output.wav
+            cat output.json
+            # break
         fi
     # fi
 done
-
+exit
 voice2json --profile ~/.local/share/voice2json/es_kaldi-rhasspy record-command > output.wav
+whisper -o . -f json --language es --model tiny ~/out.wav
